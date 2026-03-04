cfad5ff321f90ce218dbd3b60058364054b1c0e7
csoe-hub <csoe@students.pitzer.edu>
2026-03-04 00:31:06 -0800

    Fix formatting of SQL query in 11.sql

--- sql/11.sql
+++ sql/11.sql
@@ -1,10 +1,10 @@
 SELECT
-f.film_id,
-f.title,
-COUNT(*) AS count
+  f.film_id,
+  f.title,
+  COUNT(*) AS count
 FROM film AS f
 JOIN inventory AS i
-ON i.film_id = f.film_id
+  ON i.film_id = f.film_id
 WHERE f.title ILIKE 'h%'
 GROUP BY f.film_id, f.title
 ORDER BY f.title DESC;
